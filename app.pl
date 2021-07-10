3302d3e6216b5d834d38c4b8067e9b45395b54c1
Kiyo Tsunezumi <kiyotune@protein.osaka-u.ac.jp>
2021-07-11 04:06:54 +0900

    implemented to output by TSV

--- app.pl
+++ app.pl
@@ -8,16 +8,16 @@ use lib $FindBin::Bin;
 use parse_lshw;
 use Data::Dumper;
 
-## create object from original filename
+# create object from original filename
 #system("sudo lshw -json > ./lshw.json");
-#my $lshw = new parse_lshw("./lshw.json");
-
-# create object from default filename (input.json)
-my $lshw = new parse_lshw("./input.json");
+my $lshw = new parse_lshw("./lshw.json");
 
 ## parse json file to hash-data
 my $data = $lshw->parse();
 #say Dumper $data;
 
-# output to json-text
-say $lshw->_to_json();
+# output to json text
+#say $lshw->_to_json();
+
+# output to tab separated text
+say $lshw->_to_tsv();
--- parse_lshw.pm
+++ parse_lshw.pm
@@ -5,12 +5,10 @@ use warnings;
 use feature qw(say);
 use utf8;
 use JSON;
-use Data::Structure::Util qw(unbless);
 use FindBin;
 use Data::Dumper;
 
 our $version = "0.9.0";
-our $DIR = $FindBin::Bin;
 
 ############################################################################
 ### new
@@ -22,7 +20,7 @@ sub new{
   if ($#_ > 0){
     $self->{input} = $_[1];
   }else{
-    $self->{input} = "$DIR/input.json";
+    $self->{input} = "$FindBin::Bin/input.json";
   }
 
   bless $self;
@@ -49,28 +47,51 @@ sub parse{
 		die("[error] cannot open file - $self->{input}");
 	}
 
+	# trimming
+	map {
+		delete($_->{capabilities});
+		delete($_->{configuration});
+		delete($_->{children});
+		delete($_->{claimed});
+	} @devices;	
+
 	return $self->{data};
 }
 
 sub get_devices {
 	my $parent = shift;
 	my $devices = shift;
-	my @include = (
+	my @includes = (
 		["firmware", "memory"],
-		["cpu:", "cpu"],
+		["cpu:", "processor"],
 		["display", "display"],
 		["bank:", "memory"],
 		["disk:", "disk"],
 		["network", "network"],
 	);
 
+	# list::includes only
+	if(grep {$parent->{id}=~/^$_->[0]/ && $parent->{class}=~/^$_->[1]/} @includes){
+		# id:no 
+		my ($id, $no) = split(/:/, $parent->{id});
+		if(!$no){
+			my @items = grep {$_->{class} eq $parent->{class} && $_->{id} eq $id} @{$devices};
+			if($#items >= 0){
+				@items = sort {$b->{no} <=> $a->{no}} @items;
+				$no = $items[0]->{no} + 1;
+			}else{
+				$no = 0;
+			}
+		}
+		$parent->{id} = $id;
+		$parent->{no} = $no;
+
+		push(@{$devices}, $parent);
+	}
+
 	if(exists($parent->{children})){
 		foreach my $child (@{$parent->{children}}){
 			get_devices($child, $devices);
-		}
-	}else{
-		if(grep {$parent->{id}=~/^$_->[0]/ && $parent->{class}=~/^$_->[1]/} @include){
-			push(@{$devices}, $parent);
 		}
 	}
 
@@ -89,4 +110,29 @@ sub _to_json {
 	return $json_text;
 }
 
+############################################################################
+### to_tsv
+############################################################################
+sub _to_tsv {
+	my $self = shift;
+	my $tsv_text = "";
+	if(exists($self->{data})){
+		my $data = $self->{data};
+		my $hostname = [keys(%{$data})]->[0];
+		
+		my $items = $data->{$hostname};
+		$tsv_text.="$hostname\tvendor\t\t$items->{vendor}\n";
+		$tsv_text.="$hostname\tproduct\t\t$items->{product}\n";
+		$tsv_text.="$hostname\tserial\t\t$items->{serial}\n";
+		
+		my $devices = $items->{devices};
+		foreach my $device (@{$devices}){
+			my $name = "$hostname\tdevices\t$device->{class}\t$device->{id}\t$device->{no}";
+			foreach my $k (grep {!/id|class|no/} keys(%{$device})){
+				$tsv_text.="$name\t$k\t$device->{$k}\n";
+			}
+		}
+	}
+	return $tsv_text;
+}
 1;
